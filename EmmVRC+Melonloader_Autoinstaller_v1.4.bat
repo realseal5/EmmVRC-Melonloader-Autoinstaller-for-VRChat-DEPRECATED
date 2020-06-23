63467b54bb7dcca949576b50cd74363559cd61eb
realseal5 <67324123+realseal5@users.noreply.github.com>
2020-06-23 16:23:35 +0200

    Add files via upload

---
+++ EmmVRC+Melonloader_Autoinstaller_v1.4.bat
@@ -0,0 +1,52 @@
+@echo off
+echo [34m
+echo ^                           __                           __    
+echo ^   _______   ____  _____  ^|  ^|    ______  ____  _____  ^|  ^|   
+echo ^   \_  __ \_/ __ \ \__  \ ^|  ^|   /  ___/_/ __ \ \__  \ ^|  ^|   
+echo ^    ^|  ^| \/\  ___/  / __ \^|  ^|__ \___ \ \  ___/  / __ \^|  ^|__ 
+echo ^    ^|__^|    \____^> (______/____//_____/  \____^> (______/____/ 
+echo [0m
+echo ^    --------------------------------------------------------
+echo ^    ^|  MelonLoader - EmmVRC AutoInstaller v1.4 by realseal ^|
+echo ^    ^|  EmmVRC by thetrueyoshifan (thetrueyoshifan.com)     ^|
+echo ^    ^|  MelonLoader by Herp Derpinstine (melonwiki.xyz)     ^|
+echo ^    --------------------------------------------------------
+echo.
+echo Checking VrChat installation..
+IF EXIST VRChat.exe (
+echo Done!
+echo Cleaning up existing MelonLoader installation..
+IF EXIST MelonLoader rd /S /Q MelonLoader
+IF EXIST version.dll del version.dll
+IF EXIST MelonLoader.zip del MelonLoader.zip
+IF EXIST NOTICE.txt del NOTICE.txt
+IF EXIST winmm.dll del winmm.dll
+IF EXIST Mono.Cecil.dll del Mono.Cecil.dll
+echo Done!
+echo Cleaning up existing EmmVRC installation..
+IF EXIST Mods\emmVRCLoader.dll del Mods\emmVRCLoader.dll
+echo Done!
+echo Downloading MelonLoader..
+powershell -Command "Invoke-WebRequest https://github.com/HerpDerpinstine/MelonLoader/releases/latest/download/MelonLoader.zip -Outfile MelonLoader.zip"
+echo Done!
+echo Installing MelonLoader..
+powershell -Command "Expand-Archive MelonLoader.zip -DestinationPath ..\VRchat"
+IF NOT EXIST Mods md  Mods
+IF EXIST MelonLoader.zip del MelonLoader.zip
+echo Done!
+echo Downloading EmmVRC..
+powershell -Command "Invoke-WebRequest https://thetrueyoshifan.com/downloads/emmVRCLoader.dll -Outfile Mods\emmVRCLoader.dll"
+echo Done!
+echo [32m
+echo ^    ----------------------------------------------------------
+echo ^    ^| Melonloader and EmmVRC have been successfully updated! ^|
+echo ^    ----------------------------------------------------------
+echo [0m
+pause
+) else (
+echo VrChat^.exe not found
+echo Please put this file in the VRChat installation folder^!
+echo Usually in^: C^:^\Program Files ^(x86^)^\Steam^\steamapps^\common^\VRChat
+echo.
+pause
+)
